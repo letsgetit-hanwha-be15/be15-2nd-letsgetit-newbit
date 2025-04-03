104935e12814fd34c4e972d7bfbc145b5bacda7a
oyk0510 <youkyungoh0510@naver.com>
2025-04-03 18:09:27 +0900

    initial commit

---
+++ src/main/java/com/newbit/NewbitApplication.java
@@ -0,0 +1,13 @@
+package com.newbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/newbit/common/dto/Pagination.java
@@ -0,0 +1,12 @@
+package com.newbit.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Pagination {
+    private final int currentPage;
+    private final int totalPage;
+    private final long totalItems;
+}
