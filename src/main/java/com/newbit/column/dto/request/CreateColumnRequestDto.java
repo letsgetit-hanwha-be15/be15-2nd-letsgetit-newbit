971825f471ed7be41b7641424f79b77fdbeff7d6
oyk0510 <youkyungoh0510@naver.com>
2025-04-05 21:40:30 +0900

    feat: NBT-78 칼럼 등록 요청 DTO 생성

---
+++ src/main/java/com/newbit/column/dto/request/CreateColumnRequestDto.java
@@ -0,0 +1,20 @@
+package com.newbit.column.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CreateColumnRequestDto {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
+
+    @NotNull(message = "가격은 필수입니다.")
+    private Integer price;
+
+    private String thumbnailUrl;
+}
