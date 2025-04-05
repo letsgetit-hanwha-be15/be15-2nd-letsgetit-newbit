8e63b1d4c1ce759a335589efb3f905aceb47de13
oyk0510 <youkyungoh0510@naver.com>
2025-04-06 01:31:19 +0900

    feat: NBT-78 ColumnRequestRepository 생성

---
+++ src/main/java/com/newbit/column/repository/ColumnRequestRepository.java
@@ -0,0 +1,8 @@
+package com.newbit.column.repository;
+
+import com.newbit.column.entity.ColumnRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColumnRequestRepository extends JpaRepository<ColumnRequest, Long> {
+
+}
