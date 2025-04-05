0a18e820117ca3949aa0a126957a842d10ecea33
oyk0510 <youkyungoh0510@naver.com>
2025-04-06 01:38:14 +0900

    feat: NBT-78 CreateColumnRequestDto에 Builder 패턴 적용

--- src/main/java/com/newbit/column/dto/request/CreateColumnRequestDto.java
+++ src/main/java/com/newbit/column/dto/request/CreateColumnRequestDto.java
@@ -2,9 +2,11 @@ package com.newbit.column.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateColumnRequestDto {
 
     @NotBlank(message = "제목은 필수입니다.")
