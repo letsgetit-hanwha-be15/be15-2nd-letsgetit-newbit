2061d10c9fb5180e2e57f70ccbafb1e16a0d7743
oyk0510 <youkyungoh0510@naver.com>
2025-04-06 14:32:23 +0900

    feat: NBT-78 ColumnMapper에 Column 생성 메서드(toColumn) 추가

--- src/main/java/com/newbit/column/mapper/ColumnMapper.java
+++ src/main/java/com/newbit/column/mapper/ColumnMapper.java
@@ -1,13 +1,26 @@
 package com.newbit.column.mapper;
 
 import com.newbit.column.dto.request.CreateColumnRequestDto;
+import com.newbit.column.entity.Column;
 import com.newbit.column.entity.ColumnRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ColumnMapper {
 
-    public ColumnRequest toEntity(CreateColumnRequestDto dto, Long mentorId) {
-        return ColumnRequest.createdColumnRequest(dto, mentorId);
+    public Column toColumn(CreateColumnRequestDto dto, Long mentorId) {
+        return Column.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .price(dto.getPrice())
+                .thumbnailUrl(dto.getThumbnailUrl())
+                .mentorId(mentorId)
+                .isPublic(false)
+                .likeCount(0)
+                .build();
+    }
+
+    public ColumnRequest toColumnRequest(CreateColumnRequestDto dto, Column column) {
+        return ColumnRequest.createdColumnRequest(dto, column);
     }
 }
