d55f30ebd9ade08a50d603a92ef0681876a08616
oyk0510 <youkyungoh0510@naver.com>
2025-04-06 18:18:32 +0900

    fix: NBT-78 201 상태 코드로 응답하도록 수정

--- src/main/java/com/newbit/column/controller/ColumnRequestController.java
+++ src/main/java/com/newbit/column/controller/ColumnRequestController.java
@@ -6,6 +6,7 @@ import com.newbit.column.service.ColumnRequestService;
 import com.newbit.common.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ public class ColumnRequestController {
     private final ColumnRequestService columnRequestService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CreateColumnResponseDto> createColumnRequest(
             @RequestBody @Valid CreateColumnRequestDto dto,
             @RequestParam Long mentorId // 프론트에서 mentorId 넘겨준다고 가정
