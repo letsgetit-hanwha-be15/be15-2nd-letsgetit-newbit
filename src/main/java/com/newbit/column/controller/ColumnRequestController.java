7e6bb037f9114ce93ef93be7973c4e94e5adbdd7
oyk0510 <youkyungoh0510@naver.com>
2025-04-07 11:48:29 +0900

    feat: NBT-79 칼럼 수정 요청 API 생성

--- src/main/java/com/newbit/column/controller/ColumnRequestController.java
+++ src/main/java/com/newbit/column/controller/ColumnRequestController.java
@@ -1,7 +1,9 @@
 package com.newbit.column.controller;
 
 import com.newbit.column.dto.request.CreateColumnRequestDto;
+import com.newbit.column.dto.request.UpdateColumnRequestDto;
 import com.newbit.column.dto.response.CreateColumnResponseDto;
+import com.newbit.column.dto.response.UpdateColumnResponseDto;
 import com.newbit.column.service.ColumnRequestService;
 import com.newbit.common.dto.ApiResponse;
 import jakarta.validation.Valid;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class ColumnRequestController {
     private final ColumnRequestService columnRequestService;
 
+    // 칼럼 등록 요청 API
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CreateColumnResponseDto> createColumnRequest(
@@ -22,5 +25,15 @@ public class ColumnRequestController {
             @RequestParam Long mentorId // 프론트에서 mentorId 넘겨준다고 가정
     ) {
         return ApiResponse.success(columnRequestService.createColumnRequest(dto, mentorId));
+    }
+
+    // 칼럼 수정 요청 API
+    @PostMapping("/{columnId}/edit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<UpdateColumnResponseDto> updateColumnRequest(
+            @PathVariable Long columnId,
+            @RequestBody @Valid UpdateColumnRequestDto dto
+            ) {
+        return ApiResponse.success(columnRequestService.updateColumnRequest(dto, columnId));
     }
 }
