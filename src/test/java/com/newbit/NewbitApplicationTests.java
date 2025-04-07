00534f4d28e8accb6a97f42fee0e07bc8677ab70
kimjy8937 <kimjy3520@gmail.com>
2025-04-07 17:12:45 +0900

    NBT-66 feat: 칼럼 내역 조회 단위 테스트 코드 작성

---
+++ src/test/java/com/newbit/NewbitApplicationTests.java
@@ -0,0 +1,11 @@
+package com.newbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NewbitApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
