8a125c748679176f3af75e9d52d0322dd9aade01
Yangha Park <64etuor@gmail.com>
2025-04-07 19:50:53 +0900

    feat:NBT-124 채팅방 생성 기능 구현

--- src/main/java/com/newbit/coffeeletter/controller/ChatController.java
+++ src/main/java/com/newbit/coffeeletter/controller/ChatController.java
@@ -1,10 +1,28 @@
 package com.newbit.coffeeletter.controller;
 
+import com.newbit.coffeeletter.dto.CoffeeLetterRoomDTO;
+import com.newbit.coffeeletter.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/coffeeletter")
 public class ChatController {
+
+    private final ChatService chatService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(ChatService chatService, SimpMessagingTemplate messagingTemplate) {
+        this.chatService = chatService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @PostMapping("/rooms")
+    public ResponseEntity<CoffeeLetterRoomDTO> createRoom(@RequestBody CoffeeLetterRoomDTO roomDto) {
+        return ResponseEntity.ok(chatService.createRoom(roomDto));
+    }
+
 }
