b286baa033cb9fcc68988286e26479d2e634f553
oyk0510 <youkyungoh0510@naver.com>
2025-04-07 17:32:13 +0900

    feat: NBT-80 DeleteColumnResponseDto 생성

---
+++ src/main/java/com/newbit/column/dto/response/DeleteColumnResponseDto.java
@@ -0,0 +1,10 @@
+package com.newbit.column.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DeleteColumnResponseDto {
+    private Long columnRequestId;
+}
