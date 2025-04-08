b55d4d617da4df1a4566c47d1327293f7d93e84c
Yangha Park <64etuor@gmail.com>
2025-04-08 10:45:58 +0900

    fix: uncommited code added

--- src/main/java/com/newbit/coffeeletter/controller/ChatController.java
+++ src/main/java/com/newbit/coffeeletter/controller/ChatController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import com.newbit.coffeeletter.domain.chat.MessageType;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newbit.coffeeletter.domain.chat.CoffeeLetterRoom;
