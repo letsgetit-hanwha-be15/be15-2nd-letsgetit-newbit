2b4c4f81d2f9a1a3d85c56e60b1ec62ae95ace2d
gyeongmin03 <gyeongmin03@naver.com>
2025-04-08 17:47:56 +0900

    feat: NBT-65 JPA 저장소

---
+++ src/main/java/com/newbit/user/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.newbit.user.repository;
+
+import com.newbit.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUserNameAndPhoneNumber(String userName, String phoneNumber);
+    Optional<User> findByEmail(String email);
+
+    default User getById(Integer userId){
+        return findById(userId).orElseThrow();
+    }
+
+    boolean existsByEmail(String email);
+}
