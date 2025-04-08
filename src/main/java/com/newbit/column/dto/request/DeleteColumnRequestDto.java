50fc81a0e33b8f98365a6a4f758ed7dc08aaa03d
oyk0510 <youkyungoh0510@naver.com>
2025-04-08 17:54:24 +0900

    feat: NBT-80 멘토 칼럼 요청(등록, 수정, 삭제) Swagger 어노테이션 추가

--- src/main/java/com/newbit/column/dto/request/DeleteColumnRequestDto.java
+++ src/main/java/com/newbit/column/dto/request/DeleteColumnRequestDto.java
@@ -1,10 +1,13 @@
 package com.newbit.column.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class DeleteColumnRequestDto {
+
+    @Schema(description = "삭제 요청 사유", example = "더 이상 제공하지 않는 칼럼입니다.")
     private String reason; // 삭제 요청 사유
 }
--- src/main/java/com/newbit/column/dto/response/UpdateColumnResponseDto.java
+++ src/main/java/com/newbit/column/dto/response/UpdateColumnResponseDto.java
@@ -1,10 +1,14 @@
 package com.newbit.column.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "칼럼 수정 요청 응답 DTO")
 public class UpdateColumnResponseDto {
+
+    @Schema(description = "생성된 수정 요청 ID", example = "5")
     private Long columnRequestId;
 }
