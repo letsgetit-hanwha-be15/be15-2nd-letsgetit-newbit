eaab6915a6a043126a8695e764afe5bb3e952778
sezeme <stellaa223@gmail.com>
2025-04-09 17:20:21 +0900

    feat:컨트롤러 생성

--- src/main/java/com/newbit/coffeechat/command/application/controller/CoffeechatCommandController.java
+++ src/main/java/com/newbit/coffeechat/command/application/controller/CoffeechatCommandController.java
@@ -1,4 +1,37 @@
 package com.newbit.coffeechat.command.application.controller;
 
+import com.newbit.coffeechat.command.application.dto.request.CoffeechatCreateRequest;
+import com.newbit.coffeechat.command.application.dto.response.CoffeechatCommandResponse;
+import com.newbit.coffeechat.command.application.service.CoffeechatCommandService;
+import com.newbit.common.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coffeechat")
 public class CoffeechatCommandController {
+
+    private final CoffeechatCommandService coffeechatCommandService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse<CoffeechatCommandResponse>> createCoffeechat(
+            @Validated CoffeechatCreateRequest coffeechatCreateRequest
+    ) {
+
+        Long coffeechatId = coffeechatCommandService.createCoffeechat(coffeechatCreateRequest);
+
+        CoffeechatCommandResponse response = CoffeechatCommandResponse.builder()
+                .coffeechatId(coffeechatId)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(response));
+    }
 }
