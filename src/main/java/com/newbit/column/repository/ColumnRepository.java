95e293e5456dcf1106e18188ac618146b13532e2
oyk0510 <youkyungoh0510@naver.com>
2025-04-10 16:45:33 +0900

    feat: NBT-156 mentor, user 조인 JPQL 작성

--- src/main/java/com/newbit/column/repository/ColumnRepository.java
+++ src/main/java/com/newbit/column/repository/ColumnRepository.java
@@ -1,10 +1,21 @@
 package com.newbit.column.repository;
 
 import com.newbit.column.domain.Column;
+import com.newbit.column.dto.response.GetColumnListResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ColumnRepository extends JpaRepository<Column, Long> {
-    Page<Column> findAllByIsPublicTrueOrderByCreatedAtDesc(Pageable pageable);
+
+    @Query("SELECT new com.newbit.column.dto.response.GetColumnListResponseDto( " +
+            "c.columnId, c.title, c.thumbnailUrl, c.price, c.likeCount, m.mentorId, u.nickname) " +
+            "FROM Column c " +
+            "JOIN c.mentor m " +
+            "JOIN m.user u " +
+            "WHERE c.isPublic = true " +
+            "ORDER BY c.createdAt DESC")
+
+    Page<GetColumnListResponseDto> findAllByIsPublicTrueOrderByCreatedAtDesc(Pageable pageable);
 }
