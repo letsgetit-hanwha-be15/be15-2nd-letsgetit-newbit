15fb7ab81753e981fd0b21f202a73b710baab788
kimjy8937 <kimjy3520@gmail.com>
2025-04-10 10:21:56 +0900

    feat: NBT-146 판매 내역 조회 Service 초기 작성

---
+++ src/main/java/com/newbit/purchase/query/service/SaleHistoryQueryService.java
@@ -0,0 +1,35 @@
+package com.newbit.purchase.query.service;
+
+import com.newbit.common.dto.Pagination;
+import com.newbit.purchase.query.dto.request.HistoryRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SaleHistoryQueryService {
+
+    private final SaleHistoryMapper saleHistoryMapper;
+
+    @Transactional(readOnly = true)
+    public SaleHistoryListResponse getSaleHistories(HistoryRequest request) {
+
+        List<SaleHistoryDto> saleHistories = saleHistoryMapper.findSaleHistories(request);
+        long totalItems = saleHistoryMapper.countSaleHistories(request);
+
+        int page = request.getPage();
+        int size = request.getSize();
+
+        return SaleHistoryListResponse.builder()
+                .histories(saleHistories)
+                .pagination(Pagination.builder()
+                        .currentPage(page)
+                        .totalPage((int)Math.ceil((double) totalItems / size))
+                        .totalItems(totalItems)
+                        .build()
+                ).build();
+    }
+}
