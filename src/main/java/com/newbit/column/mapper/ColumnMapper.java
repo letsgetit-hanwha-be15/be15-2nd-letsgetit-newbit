9f22bfebd5617f476a931a91c391ce81d3b3de29
oyk0510 <youkyungoh0510@naver.com>
2025-04-11 00:36:09 +0900

    feat: NBT-161 본인 칼럼 요청 목록 응답을 위한 DTO 변환 메서드 추가

--- src/main/java/com/newbit/column/mapper/ColumnMapper.java
+++ src/main/java/com/newbit/column/mapper/ColumnMapper.java
@@ -3,6 +3,7 @@ package com.newbit.column.mapper;
 import com.newbit.column.dto.request.CreateColumnRequestDto;
 import com.newbit.column.domain.Column;
 import com.newbit.column.domain.ColumnRequest;
+import com.newbit.column.dto.response.GetMyColumnRequestResponseDto;
 import com.newbit.column.enums.RequestType;
 import com.newbit.user.entity.Mentor;
 import org.springframework.stereotype.Component;
@@ -31,6 +32,18 @@ public class ColumnMapper {
                 .updatedPrice(dto.getPrice())
                 .updatedThumbnailUrl(dto.getThumbnailUrl())
                 .column(column)
+                .build();
+    }
+
+    public GetMyColumnRequestResponseDto toMyColumnRequestResponseDto(ColumnRequest columnRequest) {
+        return GetMyColumnRequestResponseDto.builder()
+                .columnRequestId(columnRequest.getColumnRequestId())
+                .requestType(columnRequest.getRequestType())
+                .isApproved(columnRequest.getIsApproved())
+                .title(columnRequest.getUpdatedTitle())
+                .price(columnRequest.getUpdatedPrice())
+                .thumbnailUrl(columnRequest.getUpdatedThumbnailUrl())
+                .createdAt(columnRequest.getCreatedAt())
                 .build();
     }
 }
