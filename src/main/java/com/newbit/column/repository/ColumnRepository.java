becf0dcb1b7bfcd502ebe6ea06fc89dd9b604f18
oyk0510 <youkyungoh0510@naver.com>
2025-04-10 11:06:35 +0900

    feat: NBT-85 공개된 칼럼 생성된 날짜 순으로 정렬

--- src/main/java/com/newbit/column/repository/ColumnRepository.java
+++ src/main/java/com/newbit/column/repository/ColumnRepository.java
@@ -3,4 +3,8 @@ package com.newbit.column.repository;
 import com.newbit.column.domain.Column;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColumnRepository extends JpaRepository<Column, Long> {}
+import java.util.List;
+
+public interface ColumnRepository extends JpaRepository<Column, Long> {
+    List<Column> findAllByIsPublicTrueOrderByCreatedAtDesc();
+}
