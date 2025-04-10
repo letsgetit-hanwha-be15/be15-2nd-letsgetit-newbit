470dde2968a8cdc642c04c2ad1ac112093be6950
sezeme <stellaa223@gmail.com>
2025-04-10 12:00:31 +0900

    feat: 에러처리 로직 추가

--- src/main/java/com/newbit/coffeechat/command/application/service/CoffeechatCommandService.java
+++ src/main/java/com/newbit/coffeechat/command/application/service/CoffeechatCommandService.java
@@ -4,21 +4,34 @@ import com.newbit.coffeechat.command.application.domain.repository.CoffeechatRep
 import com.newbit.coffeechat.command.application.dto.request.CoffeechatCreateRequest;
 import com.newbit.coffeechat.command.domain.aggregate.Coffeechat;
 import com.newbit.coffeechat.command.domain.repository.ProductRepository;
+import com.newbit.coffeechat.query.dto.request.CoffeechatSearchRequest;
+import com.newbit.coffeechat.query.dto.response.CoffeechatDto;
+import com.newbit.coffeechat.query.service.CoffeechatQueryService;
+import com.newbit.common.exception.BusinessException;
+import com.newbit.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CoffeechatCommandService {
 
     private final CoffeechatRepository coffeechatRepository;
+    private final CoffeechatQueryService coffeechatQueryService;
 
     /* 커피챗 등록 */
     @Transactional
     public Long createCoffeechat(Long userId, CoffeechatCreateRequest request) {
-        // 1. 커피챗 등록
+        // 1. 진행중인 커피챗이 존재
+        CoffeechatSearchRequest coffeechatSearchRequest = new CoffeechatSearchRequest();
+        List<CoffeechatDto> coffeechatDtos = coffeechatQueryService.getProgressingCoffeechats(userId, request.getMentorId(), coffeechatSearchRequest);
+        if(!coffeechatDtos.isEmpty()) throw new BusinessException(ErrorCode.COFFEECHAT_ALREADY_EXIST);
+
+        // 2.. 커피챗 등록
         Coffeechat newCoffeechat = Coffeechat.of(userId, request.getMentorId(), request.getRequestMessage(), request.getPurchaseQuantity());
 
         Coffeechat coffeechat = coffeechatRepository.save(newCoffeechat);
