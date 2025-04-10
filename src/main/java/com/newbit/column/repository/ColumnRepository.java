c382bab787f3383d0af0c795d3522d414f5cbb7d
oyk0510 <youkyungoh0510@naver.com>
2025-04-10 15:29:14 +0900

    feat: NBT-156 칼럼 목록 최신날짜 순으로 조회

--- src/main/java/com/newbit/column/repository/ColumnRepository.java
+++ src/main/java/com/newbit/column/repository/ColumnRepository.java
@@ -1,6 +1,10 @@
 package com.newbit.column.repository;
 
 import com.newbit.column.domain.Column;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColumnRepository extends JpaRepository<Column, Long> {}
+public interface ColumnRepository extends JpaRepository<Column, Long> {
+    Page<Column> findAllByIsPublicTrueOrderByCreatedAtDesc(Pageable pageable);
+}
