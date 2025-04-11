4fba543e16ca2d2b19d2b6c44a6b4de56dd2bae3
gyeongmin03 <gyeongmin03@naver.com>
2025-04-11 12:08:15 +0900

    feat: NBT-120 controller, swagger

--- src/main/java/com/newbit/user/controller/UserInfoController.java
+++ src/main/java/com/newbit/user/controller/UserInfoController.java
@@ -1,16 +1,16 @@
 package com.newbit.user.controller;
 
+import com.newbit.user.dto.request.UserInfoUpdateRequestDTO;
 import com.newbit.user.dto.response.UserDTO;
 import com.newbit.user.service.UserInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User", description = "정보 조회 (나의 프로필, 다른 사용자 프로필)")
 @RestController
@@ -20,12 +20,18 @@ public class UserInfoController {
 
     private final UserInfoService userInfoService;
 
-    @Operation(summary = "회원 정보 조회", description = "회원 정보 조회 기능")
-    @GetMapping("/me")
+    @Operation(summary = "회원 정보 조회", description = "내 프로필 조회")
+    @GetMapping("/myprofile")
     public ResponseEntity<UserDTO> getMyInfo(@AuthenticationPrincipal UserDetails userDetails) {
         String email = userDetails.getUsername(); // 현재 로그인한 사용자의 이메일
         UserDTO myInfo = userInfoService.getMyInfo(email); // 서비스로 위임
         return ResponseEntity.ok(myInfo); // 사용자 정보 반환
+    }
+
+    @Operation(summary = "회원 정보 수정", description = "내 프로필 정보 수정")
+    @PutMapping("/myprofile-modify")
+    public ResponseEntity<UserDTO> updateMyInfo(@RequestBody @Valid UserInfoUpdateRequestDTO request) {
+        return ResponseEntity.ok(userInfoService.updateMyInfo(request));
     }
 }
 
