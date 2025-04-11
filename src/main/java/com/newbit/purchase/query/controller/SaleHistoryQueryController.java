b67c9ecf4fcaaef77aba3e14170232c88f2c7d38
kimjy8937 <kimjy3520@gmail.com>
2025-04-11 11:02:21 +0900

    feat: NBT-178 SaleHistoryQueryController 클래스 수정

--- src/main/java/com/newbit/purchase/query/controller/SaleHistoryQueryController.java
+++ src/main/java/com/newbit/purchase/query/controller/SaleHistoryQueryController.java
@@ -1,6 +1,7 @@
 package com.newbit.purchase.query.controller;
 
 
+import com.newbit.auth.model.CustomUser;
 import com.newbit.common.dto.ApiResponse;
 import com.newbit.purchase.query.dto.request.HistoryRequest;
 import com.newbit.purchase.query.dto.response.ColumnPurchaseHistoryListResponse;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "구매관련 API", description = "멘토 판매 내역 조회 API")
@@ -22,11 +24,11 @@ public class SaleHistoryQueryController {
     private final SaleHistoryQueryService saleHistoryQueryService;
 
     @Operation(summary = "판매 내역 조회", description = "특정 멘토의 판매 내역을 조회합니다.")
-    @GetMapping("/{userId}")
+    @GetMapping
     public ResponseEntity<ApiResponse<SaleHistoryListResponse>> getSaleHistory(
-            @Parameter(description = "조회할 유저 ID", required = true) @PathVariable Long userId,
+            @AuthenticationPrincipal CustomUser customUser,
             @ModelAttribute HistoryRequest requestDto) {
-        requestDto.setUserId(userId);
+        requestDto.setUserId(customUser.getUserId());
         SaleHistoryListResponse response = saleHistoryQueryService.getSaleHistories(requestDto);
 
         return ResponseEntity.ok(ApiResponse.success(response));
