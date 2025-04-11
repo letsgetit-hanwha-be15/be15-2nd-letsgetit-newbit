274d315d878ffc736fe692f849c6cd845b5ac98b
kimjy8937 <kimjy3520@gmail.com>
2025-04-11 17:50:32 +0900

    feat: NBT-150 알림 관련 swagger annotation 추가

--- src/main/java/com/newbit/notification/query/controller/NotificationQueryController.java
+++ src/main/java/com/newbit/notification/query/controller/NotificationQueryController.java
@@ -5,6 +5,7 @@ import com.newbit.common.dto.ApiResponse;
 import com.newbit.notification.command.application.dto.response.NotificationSendResponse;
 import com.newbit.notification.query.dto.NotificationResponse;
 import com.newbit.notification.query.service.NotificationQueryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ public class NotificationQueryController {
 
     private final NotificationQueryService notificationQueryService;
 
+    @Operation(summary = "알림 목록 조회", description = "해당 유저의 알림 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<NotificationResponse>>> getNotificationList(
             @AuthenticationPrincipal CustomUser customUser
