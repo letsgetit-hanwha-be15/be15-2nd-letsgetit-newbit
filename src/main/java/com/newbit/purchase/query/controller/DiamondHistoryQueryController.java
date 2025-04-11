cd2cbab6c860953b3b08b91a716eed13dd140d58
kimjy8937 <kimjy3520@gmail.com>
2025-04-11 11:01:13 +0900

    feat: NBT-178 DiamondHistoryQueryController 클래스 수정

--- src/main/java/com/newbit/purchase/query/controller/DiamondHistoryQueryController.java
+++ src/main/java/com/newbit/purchase/query/controller/DiamondHistoryQueryController.java
@@ -1,6 +1,7 @@
 // DiamondHistoryQueryController.java
 package com.newbit.purchase.query.controller;
 
+import com.newbit.auth.model.CustomUser;
 import com.newbit.common.dto.ApiResponse;
 import com.newbit.purchase.query.dto.request.HistoryRequest;
 import com.newbit.purchase.query.dto.response.AssetHistoryListResponse;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "구매관련 API", description = "사용자 다이아 내역 조회 API")
@@ -21,11 +23,11 @@ public class DiamondHistoryQueryController {
     private final DiamondHistoryQueryService diamondHistoryQueryService;
 
     @Operation(summary = "다이아 내역 조회", description = "지정된 사용자의 다이아 사용/획득 내역을 조회합니다.")
-    @GetMapping("/{userId}")
+    @GetMapping
     public ResponseEntity<ApiResponse<AssetHistoryListResponse>> getDiamondHistories(
-            @Parameter(description = "조회할 유저 ID", required = true) @PathVariable Long userId,
+            @AuthenticationPrincipal CustomUser customUser,
             @ModelAttribute HistoryRequest requestDto) {
-        requestDto.setUserId(userId);
+        requestDto.setUserId(customUser.getUserId());
         AssetHistoryListResponse response = diamondHistoryQueryService.getDiamondHistories(requestDto);
 
         return ResponseEntity.ok(ApiResponse.success(response));
