62c9ddb2934364347b6507d414bb7abbc55ba323
oyk0510 <youkyungoh0510@naver.com>
2025-04-12 15:41:19 +0900

    feat: NBT-189 시리즈 목록 조회(본인 전용) repository 생성

--- src/main/java/com/newbit/column/repository/SeriesRepository.java
+++ src/main/java/com/newbit/column/repository/SeriesRepository.java
@@ -3,5 +3,8 @@ package com.newbit.column.repository;
 import com.newbit.column.domain.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeriesRepository extends JpaRepository<Series, Long> {
+    List<Series> findAllByMentor_MentorIdOrderByCreatedAtDesc(Long mentorId);
 }
