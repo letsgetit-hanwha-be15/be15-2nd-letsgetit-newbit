4f9a240374f20cbc6687299d471d600188946af6
gyeongmin03 <gyeongmin03@naver.com>
2025-04-12 18:44:23 +0900

    feat: NBT-158 사용자 프로필 조회 mapper

--- src/main/java/com/newbit/user/mapper/UserMapper.java
+++ src/main/java/com/newbit/user/mapper/UserMapper.java
@@ -1,10 +1,20 @@
 package com.newbit.user.mapper;
 
-import com.newbit.user.dto.response.OhterUserProfileDTO;
+import com.newbit.user.dto.response.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
     OhterUserProfileDTO getOhterUserProfile(@Param("userId") Long userId);
+
+    MentorProfileDTO findMentorProfile(@Param("mentorId") Long mentorId);
+
+    List<PostDTO> findUserPosts(@Param("userId") Long userId);
+    List<PostDTO> findMentorPosts(Long mentorId);
+    List<ColumnDTO> findMentorColumns(Long mentorId);
+    List<SeriesDTO> findMentorSeries(Long mentorId);
+
 }
