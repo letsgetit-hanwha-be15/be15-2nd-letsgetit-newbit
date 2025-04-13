bab02f47c3fa9eef527714e986a13f3c5f15ddf3
oyk0510 <youkyungoh0510@naver.com>
2025-04-13 17:06:57 +0900

    feat: NBT-183 멘토 정산 내역 목록 조회 repository 생성(페이징)

--- src/main/java/com/newbit/settlement/repository/MonthlySettlementHistoryRepository.java
+++ src/main/java/com/newbit/settlement/repository/MonthlySettlementHistoryRepository.java
@@ -1,7 +1,10 @@
 package com.newbit.settlement.repository;
 
 import com.newbit.settlement.entity.MonthlySettlementHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MonthlySettlementHistoryRepository extends JpaRepository<MonthlySettlementHistory, Long> {
+    Page<MonthlySettlementHistory> findAllByMentor_MentorId(Long mentorId, Pageable pageable);
 }
