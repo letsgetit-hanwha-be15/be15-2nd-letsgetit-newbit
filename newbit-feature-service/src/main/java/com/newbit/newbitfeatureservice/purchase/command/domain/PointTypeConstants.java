53145a6b59fc522fda117e58913dab6defeca5fb
kimjy8937 <kimjy3520@gmail.com>
2025-04-23 12:21:13 +0900

    feat: NBT-267 PointTypeConstants 수정

--- newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/purchase/command/domain/PointTypeConstants.java
+++ newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/purchase/command/domain/PointTypeConstants.java
@@ -11,6 +11,6 @@ public final class PointTypeConstants {
     public static final String FIRST_LOGIN = "첫 로그인 적립";
     public static final String MENTOR_AUTHORITY_PURCHASE = "멘토권 구매";
     public static final String REVIEW = "커피챗 리뷰 적립";
-    public static final String RECEIVE_TIPS = "팁 %d 수령";
-    public static final String TIPS_PROVIDED = "팁 %d 제공";
+    public static final String RECEIVE_TIPS = "팁 %d수령";
+    public static final String TIPS_PROVIDED = "팁 %d제공";
 }
