435a4c5929f2a3762ea026bbebdbff32f33b6802
oyk0510 <youkyungoh0510@naver.com>
2025-04-26 19:28:37 +0900

    feat: NBT-275 S3Uploader 파일 생성

---
+++ newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/s3/service/S3Uploader.java
@@ -0,0 +1,43 @@
+package com.newbit.newbitfeatureservice.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile file, String dirName) {
+        String fileName = dirName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        try {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new RuntimeException("S3 파일 업로드에 실패했습니다.", e);
+        }
+
+        return amazonS3.getUrl(bucket, fileName).toString(); // 업로드된 파일의 URL 반환
+    }
+
+    public void delete(String fileUrl) {
+        String key = fileUrl.substring(fileUrl.indexOf(bucket) + bucket.length() + 1); // 버킷 이후 경로 추출
+        amazonS3.deleteObject(bucket, key);
+    }
+}
