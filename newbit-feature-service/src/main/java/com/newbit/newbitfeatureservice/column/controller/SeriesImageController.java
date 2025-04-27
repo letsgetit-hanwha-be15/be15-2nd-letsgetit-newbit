28e3cd16485f4a2a040e9e2b8ab7f6695794bee2
oyk0510 <youkyungoh0510@naver.com>
2025-04-27 18:22:20 +0900

    feat: NBT-276 칼럼, 시리즈 이미지 Swagger 추가

--- newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/column/controller/SeriesImageController.java
+++ newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/column/controller/SeriesImageController.java
@@ -1,6 +1,8 @@
 package com.newbit.newbitfeatureservice.column.controller;
 
 import com.newbit.newbitfeatureservice.column.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +11,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1/series/images")
 @RequiredArgsConstructor
+@Tag(name = "시리즈 이미지 API", description = "시리즈 이미지 업로드 관련 API")
 public class SeriesImageController {
 
     private final ImageService imageService;
 
+    @Operation(summary = "시리즈 썸네일 이미지 업로드", description = "시리즈 썸네일 이미지를 S3에 업로드합니다.")
     @PostMapping("/thumbnail")
     public ResponseEntity<String> uploadSeriesThumbnail(@RequestPart MultipartFile file) {
         String imageUrl = imageService.uploadSeriesThumbnail(file);
