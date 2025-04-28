a4fc6d51753cb5073fe7251292b3c2c0bc5158f6
kihongkim <clevermind2@naver.com>
2025-04-28 12:21:24 +0900

    변경사항: 단일 -> 다중 이미지 파일 업로드

--- newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/post/service/PostImageService.java
+++ newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/post/service/PostImageService.java
@@ -5,14 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class PostImageService {
 
     private final S3Uploader s3Uploader;
 
-    public String uploadImage(MultipartFile file) {
-        return s3Uploader.upload(file, "posts");
+    public List<String> uploadImages(List<MultipartFile> files) {
+        return files.stream()
+                .map(file -> s3Uploader.upload(file, "posts"))
+                .collect(Collectors.toList());
     }
 
     public void deleteImage(String fileUrl) {
