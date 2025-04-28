4ad7abfcdcbaa721cf22b776a55509f9dd6bd224
gyeongmin03 <gyeongmin03@naver.com>
2025-04-28 22:17:06 +0900

    refactor: NBT-290 유저 계정 정보 DTO

--- newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/request/UserInfoUpdateRequestDTO.java
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/request/UserInfoUpdateRequestDTO.java
@@ -5,15 +5,10 @@ import lombok.Getter;
 
 @Getter
 public class UserInfoUpdateRequestDTO {
-    @NotBlank(message = "닉네임은 필수입니다.")
-    private String nickname;
-
-    @NotBlank(message = "이름은 필수입니다.")
-    private String jobId;
 
     @NotBlank(message = "전화번호는 필수입니다.")
     private String phoneNumber;
 
-    private String profileImageUrl;
-
+    private String currentPassword;
+    private String newPassword;
 }
