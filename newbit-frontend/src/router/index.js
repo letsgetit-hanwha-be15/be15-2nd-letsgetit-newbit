a1a1d1ad4f322dede02fdce509d50862faaea094
kimjy8937 <kimjy3520@gmail.com>
2025-04-29 19:26:54 +0900

    feat: NBT-302 router/index.js에 Layout 적용

--- newbit-frontend/src/router/index.js
+++ newbit-frontend/src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import {mainRoutes} from "@/features/main/router.js";
+import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    ...mainRoutes,
+    {
+      path: '/',
+      name: 'home',
+      component: DefaultLayout,
+      children: [
+        ...mainRoutes,
+      ]
+    }
   ],
 })
 
