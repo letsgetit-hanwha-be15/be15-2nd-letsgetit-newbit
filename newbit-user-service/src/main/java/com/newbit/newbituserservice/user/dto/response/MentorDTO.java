2d980a408e68ba0acd74971e86f8be3a03596576
gyeongmin03 <gyeongmin03@naver.com>
2025-04-30 16:38:50 +0900

    refactor: NBT-311 멘토 정보 조회 DTO

--- newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/response/MentorDTO.java
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/response/MentorDTO.java
@@ -3,6 +3,8 @@ package com.newbit.newbituserservice.user.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -11,6 +13,13 @@ import lombok.*;
 @ToString
 @Schema(description = "멘토 DTO")
 public class MentorDTO {
-    Boolean isActive;
-    Integer price;
+    private Boolean isActive;
+    private Integer price;
+    private String preferredTime;
+    private BigDecimal temperature;
+    private String nickname;
+    private String profileImageUrl;
+    private String jobName;
+    private String introduction;
+    private String externalLinkUrl;
 }
