19ad659e1ccb427070cb976060de781d413b6995
oyk0510 <youkyungoh0510@naver.com>
2025-05-01 16:46:17 +0900

    feat: NBT-319 사용되지 않는 import문 삭제

--- newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/column/controller/SeriesController.java
+++ newbit-feature-service/src/main/java/com/newbit/newbitfeatureservice/column/controller/SeriesController.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
