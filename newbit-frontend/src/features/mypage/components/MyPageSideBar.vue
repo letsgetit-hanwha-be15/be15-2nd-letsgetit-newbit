25882391cd5bf818326459440ae85f48f7675fdf
kimjy8937 <kimjy3520@gmail.com>
2025-05-02 12:15:14 +0900

    feat: NBT-322 MyPageSideBar tailwind로 변경

--- newbit-frontend/src/features/mypage/components/MyPageSideBar.vue
+++ newbit-frontend/src/features/mypage/components/MyPageSideBar.vue
@@ -1,18 +1,41 @@
+<template>
+  <aside class="w-[214px] border border-[--newbitnormal] rounded-[8px] p-6 bg-[--newbitbackground] shrink-0">
+    <div
+        v-for="section in sidebarSections"
+        :key="section.title"
+        class="mb-8"
+    >
+      <div class="text-13px-regular text-[--newbitgray] mb-3">
+        {{ section.title }}
+      </div>
+      <ul class="list-none p-0 m-0">
+        <SideBarItem
+            v-for="item in section.items"
+            :key="item.text"
+            :to="item.to"
+            :text="item.text"
+        />
+      </ul>
+    </div>
+  </aside>
+</template>
+
 <script setup>
 import SideBarItem from '@/features/mypage/components/SideBarItem.vue'
-import {computed} from "vue";
+import { computed } from 'vue'
+
 const props = defineProps({
   authority: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 })
 
 const sidebarSections = computed(() => {
   const commonSections = [
     {
       title: '마이페이지',
-      items: [{ text: '프로필', to: '/mypage/profile/edit' }]
+      items: [{ text: '프로필', to: '/mypage/profile/edit' }],
     },
     {
       title: '내 콘텐츠',
@@ -21,14 +44,14 @@ const sidebarSections = computed(() => {
         { text: '좋아요한 콘텐츠', to: '/mypage/contents?type=likes' },
         { text: '구매한 칼럼', to: '/mypage/contents?type=purchased-columns' },
         { text: '구독한 시리즈', to: '/mypage/contents?type=subscribed-series' },
-      ]
+      ],
     },
     {
       title: '커피챗',
       items: [
         { text: '커피챗 내역', to: '/mypage/history/coffeechat' },
         { text: '내 리뷰', to: '/mypage/reviews' },
-      ]
+      ],
     },
     {
       title: '활동 내역',
@@ -36,11 +59,11 @@ const sidebarSections = computed(() => {
         { text: '포인트 내역', to: '/mypage/history/point' },
         { text: '다이아 내역', to: '/mypage/history/diamond' },
         { text: '결제 내역', to: '/mypage/history/payment' },
-      ]
+      ],
     },
     {
       title: '설정',
-      items: [{ text: '계정 정보', to: '/mypage/account' }]
+      items: [{ text: '계정 정보', to: '/mypage/account' }],
     },
   ]
 
@@ -54,9 +77,9 @@ const sidebarSections = computed(() => {
         { text: '커피챗 관리', to: '/mypage/mentor/coffeechats' },
         { text: '판매 내역', to: '/mypage/history/sale' },
         { text: '월별 정산 내역', to: '/mypage/history/settlement' },
-        { text: '칼럼 요청 내역', to: '/mypage/mentor/column-requests' }
-      ]
-    }
+        { text: '칼럼 요청 내역', to: '/mypage/mentor/column-requests' },
+      ],
+    },
   ]
 
   return props.authority !== 'MENTOR'
@@ -64,44 +87,3 @@ const sidebarSections = computed(() => {
       : commonSections
 })
 </script>
-
-<template>
-  <aside class="sidebar">
-    <div v-for="section in sidebarSections" :key="section.title" class="section">
-      <div class="section-title text-13px-regular">{{ section.title }}</div>
-      <ul class="item-list">
-        <SideBarItem
-            v-for="item in section.items"
-            :key="item.text"
-            :to="item.to"
-            :text="item.text"
-        />
-      </ul>
-    </div>
-  </aside>
-</template>
-
-<style scoped>
-.sidebar {
-  width: 214px;
-  border: 1px solid var(--newbitnormal);
-  border-radius: 8px;
-  padding: 24px;
-  background-color: var(--newbitbackground);
-}
-
-.section {
-  margin-bottom: 32px;
-}
-
-.section-title {
-  color: var(--newbitgray);
-  margin-bottom: 12px;
-}
-
-.item-list {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-</style>
