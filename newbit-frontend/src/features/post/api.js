f9ebc11bc917f90ce20b64237b8e9596c4dd0e7b
kihongkim <clevermind2@naver.com>
2025-05-02 11:30:21 +0900

    게시글 목록 페이지 구현

--- newbit-frontend/src/features/post/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export async function fetchPosts() {
-    const res = await axios.get('http://localhost:8080/api/posts')
-    return res.data
-}
--- newbit-frontend/src/router/index.js
+++ newbit-frontend/src/router/index.js
@@ -7,8 +7,23 @@ import { reportRoutes } from "@/features/report/router.js";
 import { perkRoutes } from "@/features/perk/router.js";
 import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 import {mypageRoutes} from "@/features/mypage/router.js";
-import {postRoutes} from "@/features/post/router.js";
 import {columnRoutes} from "@/features/column/router.js";
+import PostListView from '@/features/post/views/PostListView.vue'
+
+
+export const postRoutes = [
+  {
+    path: '/posts',
+    name: 'PostList',
+    component: PostListView,
+  },
+  {
+    path: '/posts/category/:categoryId',
+    name: 'PostByCategory',
+    component: PostListView,
+    props: true
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
