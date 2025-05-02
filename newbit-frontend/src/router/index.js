7ef0816f9b9bba372f328d5b46cf6e0241e24308
sezeme <stellaa223@gmail.com>
2025-05-02 11:49:15 +0900

    feat: 뼈대 구성 완료

--- newbit-frontend/src/router/index.js
+++ newbit-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { mainRoutes } from "@/features/main/router.js";
 import { coffeeletterRoutes } from "@/features/coffeeletter/router.js";
+import { coffeechatRoutes } from "@/features/coffeechat/router.js";
 import { paymentRoutes } from "@/features/payment/router.js";
 import { productRoutes } from "@/features/product/router.js";
 import { reportRoutes } from "@/features/report/router.js";
@@ -8,6 +9,7 @@ import { perkRoutes } from "@/features/perk/router.js";
 import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 import {mypageRoutes} from "@/features/mypage/router.js";
 import {columnRoutes} from "@/features/column/router.js";
+import {profileRouters} from "@/features/profile/router.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +19,14 @@ const router = createRouter({
       component: DefaultLayout,
       children: [
         ...mainRoutes,
+        ...coffeechatRoutes,
         ...coffeeletterRoutes,
         ...paymentRoutes,
         ...productRoutes,
         ...reportRoutes,
         ...perkRoutes,
         ...columnRoutes,
+        ...profileRouters,
       ],
     },
     ...mypageRoutes
