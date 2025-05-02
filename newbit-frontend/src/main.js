708972f6a3f9d0dcf9f8daebbfeaa3963d3c9201
oyk0510 <youkyungoh0510@naver.com>
2025-05-02 15:41:26 +0900

    feat: NBT-343 vue-toastification 추가

--- newbit-frontend/src/main.js
+++ newbit-frontend/src/main.js
@@ -10,10 +10,14 @@ import '@/assets/styles/text-utilities.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(Toast)
 
 app.mount('#app')
