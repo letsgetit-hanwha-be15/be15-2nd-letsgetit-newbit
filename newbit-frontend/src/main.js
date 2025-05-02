41019eea600a744a6a4a0e63a1d7cf36bbd43da5
oyk0510 <youkyungoh0510@naver.com>
2025-05-02 15:20:51 +0900

    feat: NBT-343 element-plus 추가

--- newbit-frontend/src/main.js
+++ newbit-frontend/src/main.js
@@ -7,9 +7,13 @@ import router from './router'
 import '@/assets/styles/global.css'
 import '@/assets/styles/text-utilities.css'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
