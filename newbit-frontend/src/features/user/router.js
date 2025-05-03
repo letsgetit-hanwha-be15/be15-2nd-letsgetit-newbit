c0b9df92cf5e06aaaed63cb1edfdc3163725d3d7
gyeongmin03 <gyeongmin03@naver.com>
2025-05-03 18:15:10 +0900

    refactor: NBT-325 router

--- newbit-frontend/src/features/user/router.js
+++ newbit-frontend/src/features/user/router.js
@@ -3,5 +3,10 @@ export const userRoutes = [
         path: '/signup',
         name: 'SignUp',
         component: () => import('@/features/user/views/SignUpView.vue')
-    }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/features/user/views/LoginView.vue')
+    },
 ];
