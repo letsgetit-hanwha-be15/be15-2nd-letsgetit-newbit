dac9b05ba07cb3115cabbb57b328dd92ed8eb204
kimjy8937 <kimjy3520@gmail.com>
2025-05-04 01:39:00 +0900

    feat: NBT-352 PurchasedColumnsView 페이지 구현

--- newbit-frontend/src/features/mypage/views/PurchasedColumnsView.vue
+++ newbit-frontend/src/features/mypage/views/PurchasedColumnsView.vue
@@ -1,9 +1,56 @@
 <script setup>
+import {ref} from 'vue';
+import ColumnCard from "@/features/column/components/ColumnCard.vue";
 
+const purchasedColumns = ref({
+  "success": true,
+  "data": {
+    "columnPurchases": [
+      {
+        "columnId": 8,
+        "columnTitle": "JPA 성능 최적화 팁",
+        "thumbnailUrl": null,
+        "price": 1000,
+        "purchasedAt": "2025-04-18T11:50:10"
+      },
+      {
+        "columnId": 9,
+        "columnTitle": "테크 스타트업에서 살아남기",
+        "thumbnailUrl": null,
+        "price": 170,
+        "purchasedAt": "2025-04-11T10:03:38"
+      },
+      {
+        "columnId": 10,
+        "columnTitle": "코딩 테스트 전략 정리",
+        "thumbnailUrl": null,
+        "price": 110,
+        "purchasedAt": "2025-04-11T10:03:38"
+      }
+    ],
+    "pagination": {
+      "currentPage": 1,
+      "totalPage": 1,
+      "totalItems": 3
+    }
+  },
+  "errorCode": null,
+  "message": null,
+  "timestamp": "2025-05-04T01:12:26.497062"
+})
 </script>
 
 <template>
-구매한 칼럼 페이지 입니다.
+  <div class="w-full max-w-4xl mx-auto p-6">
+    <h2 class="text-heading3 mb-4">구매한 칼럼</h2>
+    <div class="space-y-6">
+      <ColumnCard
+          v-for="column in purchasedColumns.data.columnPurchases"
+          :key="column.columnId"
+          :column="column"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
