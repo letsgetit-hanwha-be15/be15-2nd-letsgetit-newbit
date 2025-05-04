60bc74554d4f2ba4ec07ef1a317ec27f0a0eb1ad
gyeongmin03 <gyeongmin03@naver.com>
2025-05-04 18:43:57 +0900

    fix: NBT-368 멘토 목록 조회 DTO

--- newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/request/MentorListRequestDTO.java
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/request/MentorListRequestDTO.java
@@ -1,18 +1,22 @@
 package com.newbit.newbituserservice.user.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class MentorListRequestDTO {
-    private String coffeechatPrice;   // "ASC" or "DESC"
-    private String temperature;       // "ASC" or "DESC"
     private String techstackName;
     private String userName;
+    private String coffeechatPrice; // "asc" or "desc"
+    private String temperature;     // "asc" or "desc"
 
-    private int page = 0;    // 기본값
-    private int size = 15;   // 기본값
+    private int page;   // 클라이언트로부터 받은 페이지 번호 (1부터 시작)
+    private int size;   // 한 페이지에 보여줄 mentor 수
+
+    // 내부 계산용
+    private int offset;
+    private int limit;
 }
