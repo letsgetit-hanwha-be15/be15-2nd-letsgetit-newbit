24f8111e15bd25451d1997dfd9d154f93fad0b74
gyeongmin03 <gyeongmin03@naver.com>
2025-05-04 15:50:42 +0900

    chore: NBT-378 bootstrap 함수로 Vue 앱 초기화

--- newbit-frontend/src/main.js
+++ newbit-frontend/src/main.js
@@ -13,11 +13,15 @@ import 'element-plus/dist/index.css'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-const app = createApp(App)
+async function bootstrap() {
+    const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-app.use(ElementPlus)
-app.use(Toast)
+    app.use(createPinia())
+    app.use(router)
+    app.use(ElementPlus)
+    app.use(Toast)
 
-app.mount('#app')
+    app.mount('#app')
+}
+
+bootstrap()
