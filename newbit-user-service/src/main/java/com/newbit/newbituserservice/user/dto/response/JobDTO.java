44b87c0fb421d60ce6c7e9e426db66476fa3759f
gyeongmin03 <gyeongmin03@naver.com>
2025-05-05 00:28:54 +0900

    feat: NBT-393 job DTO

---
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/user/dto/response/JobDTO.java
@@ -0,0 +1,11 @@
+package com.newbit.newbituserservice.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JobDTO {
+    private Long jobId;
+    private String jobName;
+}
