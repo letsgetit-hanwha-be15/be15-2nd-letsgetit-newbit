e3c8a22776d159c990a8a203e2ca5c8fdf13be2e
kimjy8937 <kimjy3520@gmail.com>
2025-05-05 20:13:09 +0900

    feat: NBT-403 회원정보 조회 api 호출 로직 작성

--- newbit-frontend/src/api/user.js
+++ newbit-frontend/src/api/user.js
@@ -5,3 +5,14 @@ import api from './axios.js'
 export function SignUpUser(data) {
     return api.post('/user/users/signup', data);
 }
+
+
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiYXV0aG9yaXR5IjoiTUVOVE9SIiwidXNlcklkIjoxMSwiaWF0IjoxNzQ2NDQzMTU1LCJleHAiOjE3NDY0NDQ5NTV9.eF9IbsNCrvXoL6bZ2uVCJVe6-7Wy-QC3HtbmOMSyiJXq2YfB82v_mr-nw2hO64qHB_ReEnE5CrB-UBQRoDCnIg'
+/* 2. 회원 정보 조회 */
+export function getUserInfo(data) {
+    return api.get('/user/users/me', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
