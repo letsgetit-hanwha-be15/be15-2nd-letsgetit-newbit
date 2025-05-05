9b130e2faa616c2a36a88afdda39689717bd0ac3
kimjy8937 <kimjy3520@gmail.com>
2025-05-05 19:54:26 +0900

    feat: NBT-397 다이아 내역 api호출 로직 작성

--- newbit-frontend/src/api/history.js
+++ newbit-frontend/src/api/history.js
@@ -28,3 +28,28 @@ export const fetchPointHistory = async (page = 1) => {
         throw error;
     }
 };
+
+
+export const fetchDiamondHistory = async (page = 1) => {
+    try {
+        // const token = localStorage.getItem('accessToken'); // 또는 Pinia, Vuex에서 가져오기
+        const response = await api.get('/feature/purchase/diamond/history', {
+            params: { page },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.data?.success) {
+            return {
+                histories: response.data.data.histories,
+                pagination: response.data.data.pagination
+            };
+        } else {
+            throw new Error('응답 형식 오류');
+        }
+    } catch (error) {
+        console.error('다이아 내역 조회 실패:', error);
+        throw error;
+    }
+};
