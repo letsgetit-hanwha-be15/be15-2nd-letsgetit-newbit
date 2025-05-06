d7d31d4387981ebc5e5f3dde0f4c87b149cfd499
kimjy8937 <kimjy3520@gmail.com>
2025-05-06 21:52:25 +0900

    feat: NBT-400 로그인시 SSE연결 하도록 설정

--- newbit-frontend/src/App.vue
+++ newbit-frontend/src/App.vue
@@ -1,5 +1,17 @@
 <script setup>
 import Footer from "@/components/common/Footer.vue";
+import {useNotificationStore} from "@/features/stores/notification.js";
+import {useAuthStore} from "@/features/stores/auth.js";
+import {onMounted} from "vue";
+
+const auth = useAuthStore();
+const notification = useNotificationStore();
+
+onMounted(() => {
+  if (auth.userId) {
+    notification.initSSE(auth.userId);
+  }
+});
 </script>
 
 <template>
