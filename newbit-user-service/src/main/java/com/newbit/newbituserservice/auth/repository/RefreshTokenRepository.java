54946456162934960c2a644101547c06fedb3c00
gyeongmin03 <gyeongmin03@naver.com>
2025-05-06 23:20:24 +0900

    refactor: NBT-441 어노테이션 추가

--- newbit-user-service/src/main/java/com/newbit/newbituserservice/auth/repository/RefreshTokenRepository.java
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/auth/repository/RefreshTokenRepository.java
@@ -2,6 +2,8 @@ package com.newbit.newbituserservice.auth.repository;
 
 import com.newbit.newbituserservice.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 }
