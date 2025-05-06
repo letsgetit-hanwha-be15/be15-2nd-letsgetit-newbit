b9b41dba9d6e12a7092a16554c0a798b88cd8f36
kimjy8937 <kimjy3520@gmail.com>
2025-05-06 14:19:56 +0900

    feat: NBT-405 구매한 칼럼 조회 api 호출 메서드에 page 추가 전달

--- newbit-frontend/src/api/purchase.js
+++ newbit-frontend/src/api/purchase.js
@@ -1,6 +1,8 @@
 import api from './axios.js'
 
 /* 1. 구매한 칼럼 조회 */
-export function getPurchaseHistory() {
-    return api.get('/feature/purchase/column/history');
+export function getPurchaseHistory(page = 1) {
+    return api.get('/feature/purchase/column/history', {
+        params: { page }
+    });
 }
