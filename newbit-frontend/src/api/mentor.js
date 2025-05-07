a1010e02b0b9e89376bc4037326c0e46b734546c
kimjy8937 <kimjy3520@gmail.com>
2025-05-07 12:09:39 +0900

    feat: NBT-450 테스트용 임시토큰 삭제

--- newbit-frontend/src/api/mentor.js
+++ newbit-frontend/src/api/mentor.js
@@ -1,29 +1,15 @@
 import api from './axios.js'
 
-// 테스트용 임시 토큰
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiYXV0aG9yaXR5IjoiTUVOVE9SIiwidXNlcklkIjoxMiwiaWF0IjoxNzQ2NDY1Njg5LCJleHAiOjE3NDY0Njc0ODl9.Q7GfPgiNB7G7SaQUw12WUqid4bs2oPktS1ALRBoBe-rY_d47HAX-eYNEjf1n-060fDPbE_U1UM4BhOxYReroTQ'
-
 /* 1. 멘토 정보 조회 */
 export const getMentorById = (mentorId) => {
-    // const token = localStorage.getItem('accessToken') // 또는 Pinia 등에서 가져오기
-
-    return api.get(`/user/mentor/${mentorId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+    return api.get(`/user/mentor/${mentorId}`);
 }
 
 /* 2. 멘토 커피챗 정보 수정 */
 export const patchMentorCoffeechatInfo = (data) => {
     return api.patch(
         '/user/users/me/coffeechat-info',
-        data,
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
+        data
     );
 };
 
@@ -31,12 +17,7 @@ export const patchMentorCoffeechatInfo = (data) => {
 export const patchMentorIntroduction = (data) => {
     return api.patch(
         '/user/users/me/introduction-info',
-        data,
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
+        data
     );
 };
 
