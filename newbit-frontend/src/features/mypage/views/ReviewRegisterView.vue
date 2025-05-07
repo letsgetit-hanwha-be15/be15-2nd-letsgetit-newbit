e8310b681ba8e0da307e1fdea3c07de54604753f
sezeme <stellaa223@gmail.com>
2025-05-08 02:01:34 +0900

    refactor : mock data 지우기

--- newbit-frontend/src/features/mypage/views/ReviewRegisterView.vue
+++ newbit-frontend/src/features/mypage/views/ReviewRegisterView.vue
@@ -9,19 +9,6 @@ import {useRoute, useRouter} from "vue-router";
 import {useToast} from "vue-toastification";
 import {createReview} from "@/api/coffeechat.js";
 
-const user = ref({
-  id: 1,
-  profileImageUrl: profileImage,
-  nickname: 'sezeme',
-  jobName: '백엔드',
-  temperature: 100,
-  price: 50,
-  preferredTime: '7시 이후 좋아요! 2시간 이하로 신청해주세요!',
-  externalLinkUrl: 'https://example.com',
-  introduction: '안녕하세요! 반갑습니다! 잘 부탁드립니다. 반갑습니다. 잘 부탁드립니다. 반갑스빈다.',
-  isActive: true
-})
-
 const isSubmitting = ref(false);
 const route = useRoute();
 const router = useRouter();
